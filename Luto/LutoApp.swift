92e03bc310211feebd8a9624c768934aab37cbde
Pierre BOULC'H <dev@pixwel.fr>
2023-07-04 19:01:13 +0200

    Initial Commit

---
+++ Luto/LutoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LutoApp.swift
+//  Luto
+//
+//  Created by Pierre Boulc'h on 04/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct LutoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
